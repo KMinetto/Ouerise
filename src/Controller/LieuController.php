9acb4e0d194b112de9496409008b9af8c5ea463e
Kilian MINETTO <killian.minetto@gmail.com>
2021-02-15 12:26:00 +0100

    Add querry with database to get one location

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -2,23 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
 use App\Service\CallApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LieuController extends AbstractController
 {
+
     /**
-     * @Route("/lieu/{lieu}", name="lieu")
-     * @param string $lieu
+     * @Route("/lieu/{id}", name="lieu", requirements={"id"="\d+"})
      * @param CallApi $callApiServices
+     * @param int $id
      * @return Response
      */
-    public function index(string $lieu, CallApi $callApiServices): Response
+    public function index(CallApi $callApiServices, int $id): Response
     {
+        $location = $this->getOneLocation($id);
+
         return $this->render('lieu/index.html.twig', [
-            'data' => $callApiServices->getApiLieu($lieu),
+            'data' => $callApiServices->getApiLieu($location)
         ]);
+    }
+
+    public function getOneLocation($id): ?string
+    {
+        $location = $this->getDoctrine()
+            ->getRepository('App:Location')
+            ->find($id);
+
+        if (!$location) {
+            throw $this->createNotFoundException('Aucun lieu n\'a été trouvé :cry:');
+        }
+
+        return $location->getName();
     }
 }
