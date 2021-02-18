2f1c51127593e59910f5aae6b604c65427fd1d87
celine-adamo <71389471+celine-adamo@users.noreply.github.com>
2021-02-18 15:01:24 +0100

    maj

--- assets/components/Home.vue
+++ assets/components/Home.vue
@@ -5,14 +5,14 @@
         <div class="p-5 rectangle">
           <div class="texte">
             <div class="col-12 d-flex">
-              <div class="col-8">
+              <div class="col-9">
                 <h3>{{ message }}</h3>
                 <p>{{ message }}</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quos neque nam vero adipisci dolore labore quasi explicabo, libero architecto necessitatibus praesentium totam est expedita? Quis aliquam velit quo sequi!Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam veritatis quisquam delectus nulla dignissimos eos, voluptatibus provident sunt illum soluta ipsum sapiente nisi! Eum, eveniet deleniti. Cum accusamus in ut.</p>
               </div>
-              <div class="col-4">
+              <div class="col-3">
                 <img
-                  class="img-fluid dim ms-4 "
+                  class="img-fluid dim ms-4"
                   src="/assets/img/palaisFacteur.jpg"
                   alt="palais"
                 />
