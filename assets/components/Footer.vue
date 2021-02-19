652b3be1b1c5d731a3dcf4386412caf78cb300fc
celine-adamo <71389471+celine-adamo@users.noreply.github.com>
2021-02-19 10:21:36 +0100

    maj

--- assets/components/Footer.vue
+++ assets/components/Footer.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
-    <!-- Footer -->
     <footer class="text-center text-lg-start">
-      <!-- Copyright -->
       <div class="text-center p-3 couleur">
-                <a class="text-light" href="https://www.mapbox.com/"
-          >API</a
-        >
+        <a class="text-light" href="https://www.mapbox.com/">API</a>
         <a class="text-light" href="https://github.com/KMinetto/Ouerise"
           >GitHub</a
         >
       </div>
-      <!-- Copyright -->
     </footer>
-    <!-- Footer -->
   </div>
 </template>
 
